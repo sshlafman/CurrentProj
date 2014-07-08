d25ee0f7bdca95fb4dc2820778823f96a5f21c21
Shymon Shlafman <sshlafman@gmail.com>
2014-07-08 08:00:26 +0300

    Started TDD

---
+++ fileproc.py
@@ -0,0 +1,2 @@
+class TextEncoder:
+    pass
---
+++ fileproctests.py
@@ -0,0 +1,11 @@
+import unittest
+from fileproc import TextEncoder
+
+class TextEncoderDefaultTargetEncodingToTestCase(unittest.TestCase):
+    def runTest(self):
+        """Check that TextEncoder is created with default target encoding"""
+        encoder = TextEncoder()
+        assert encoder.targetEncoding() == 'utf8', 'incorrect default target encoding'
+        
+if __name__ == "__main__":
+    unittest.main()
