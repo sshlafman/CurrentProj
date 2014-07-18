3148f1bdd8826c92d8cb00ea027bf46b5e33c1f9
Shymon Shlafman <sshlafman@gmail.com>
2014-07-18 18:54:02 +0300

    Grouped similar test function into test cases (TextConverter)

--- fileproctests.py
+++ fileproctests.py
@@ -1,44 +1,8 @@
 import unittest
 from fileproc import TextConverter
 
-class TextConverterDefaultTargetEncodingToTestCase(unittest.TestCase):
-    def runTest(self):
-        """Check that TextConverter is created with default target encoding"""
-        converter = TextConverter('866')
-        self.assertEqual(converter.targetEncoding, 'utf8', 'incorrect default target encoding') 
-        
-class TextConverterNoSourceEncodingExceptionTestCase(unittest.TestCase):
-    def runTest(self):
-        """Check that TextConverter throws exception if no source encoding passed to cnstr"""
-        try:
-            TextConverter('')
-        except ValueError:
-            pass
-        else:
-            self.fail("expected exception ValueError")
-
-class TextConverterInvalidSourceEncodingRaisesExceptionTestCase(unittest.TestCase):
-    def runTest(self):
-        """Check that TextConverter throws correct exception if source encoding is invalid"""
-        try:
-            TextConverter('someencoding', 'utf8')
-        except LookupError:
-            pass
-        else:
-            self.fail("expected exception LookupError")
-
-class TextConverterInvalidTargetEncodingRaisesExceptionTestCase(unittest.TestCase):
-    def runTest(self):
-        """Check that TextConverter throws correct exception if target encoding is invalid"""
-        try:
-            TextConverter('utf8', 'someencoding')
-        except LookupError:
-            pass
-        else:
-            self.fail("expected exception LookupError")
-
-class TextConverterInvalidEncodingsRaiseExceptionTestCase(unittest.TestCase):
-    def runTest(self):
+class TextConverterCheckForInvalidArgumentsTestCase(unittest.TestCase):
+    def test_invalid_arguments_raise_exception(self):
         """Check that TextConverter throws correct exception if both encodings are invalid"""
         try:
             TextConverter('someencoding', 'anotherencoding')
@@ -46,34 +10,63 @@ class TextConverterInvalidEncodingsRaiseExceptionTestCase(unittest.TestCase):
             pass
         else:
             self.fail("expected exception LookupError")
+            
+    def test_invalid_target_encoding_raises_exception(self):
+        """Check that TextConverter throws correct exception if target encoding is invalid"""
+        try:
+            TextConverter('utf8', 'someencoding')
+        except LookupError:
+            pass
+        else:
+            self.fail("expected exception LookupError")
+        
+    def test_invalid_source_encoding_raises_exception(self):
+        """Check that TextConverter throws correct exception if source encoding is invalid"""
+        try:
+            TextConverter('someencoding', 'utf8')
+        except LookupError:
+            pass
+        else:
+            self.fail("expected exception LookupError")
+    
+    def test_no_source_encoding_raises_exception(self):
+        """Check that TextConverter throws exception if no source encoding passed to constructor"""
+        try:
+            TextConverter('')
+        except ValueError:
+            pass
+        else:
+            self.fail("expected exception ValueError")
 
-class TextConverterValidateArgumentEncodingsTestCase(unittest.TestCase):
+class TextConverterDefaultTargetEncodingToTestCase(unittest.TestCase):
     def runTest(self):
+        """Check that TextConverter is created with default target encoding"""
+        converter = TextConverter('866')
+        self.assertEqual(converter.targetEncoding, 'utf8', 'incorrect default target encoding') 
+
+class TextConverterConversionTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sourceEncoding = '866'
+        self.targetEncoding = 'utf16'
+        self.converter = TextConverter(self.sourceEncoding, self.targetEncoding)
+              
+    def test_validate_arguments(self):
         """Check that TextConverter is created with correct encodings"""
-        sourceEncoding = '866'
-        targetEncoding = 'utf16'
-        converter = TextConverter(sourceEncoding, targetEncoding)
-        self.assertEqual(converter.encodings(), (sourceEncoding, targetEncoding), 
+        self.assertEqual(self.converter.encodings(), 
+                         (self.sourceEncoding, self.targetEncoding), 
                          'incorrect source encoding')
-        
-class TextConverterInputTextIsBytesStringTestCase(unittest.TestCase):
-    def runTest(self):
+    
+    def test_convert_accepts_bytes_string(self):
         """Check that text passed to convert() is a bytes string"""
-        converter = TextConverter('866', 'utf16')
         text = 'regular string'
-        self.assertRaises(ValueError, converter.convert, text)
-        
-class TextConverterValidateCorrectConvertionTestCase(unittest.TestCase):
-    def runTest(self):
+        self.assertRaises(ValueError, self.converter.convert, text)
+    
+    def test_convert_produces_correct_output(self):        
         """Check that conversion is correct"""
-        sourceEncoding = '866'
-        targetEncoding = 'utf16'
-        testText = 'Летят утки'
-        
-        converter = TextConverter(sourceEncoding, targetEncoding)
-        inBytes = bytes(testText, encoding=sourceEncoding)
-        outBytes = converter.convert(inBytes)
-        self.assertEqual(outBytes.decode(targetEncoding), testText, 'incorrect output in convert()')          
+        testText = 'Летят утки'        
+        inBytes = bytes(testText, encoding=self.sourceEncoding)
+        outBytes = self.converter.convert(inBytes)
+        self.assertEqual(outBytes.decode(self.targetEncoding), testText, 'incorrect output in convert()')          
         
 if __name__ == "__main__":
     unittest.main()
